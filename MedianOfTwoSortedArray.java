16847e875b10401cffb53b72265ae69e5dfa2ba1
YaswanthMallavarapu <mallavarapuyaswanth5@gmail.com>
2024-12-26 17:43:12 +0530

    Median of two sorted arrays solution is added

---
+++ MedianOfTwoSortedArray.java
@@ -0,0 +1,74 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    //     int n1=nums1.length;
+    //     int n2=nums2.length;
+    //     int nums[]=new int[n1+n2];
+    //     int n=n1+n2;
+    //     int i=0,j=0,k=0;
+    //     int indx1=n/2;
+    //     int indx2=indx1-1;
+    //     int ele1=-1,ele2=-1;
+    //     int cnt=0;
+    //     while(i<n1 && j<n2){
+    //         if(nums1[i]<nums2[j]){
+    //            if(cnt==indx1)ele1=nums1[i];
+    //            if(cnt==indx2)ele2=nums1[i];
+    //            cnt++;
+    //            i++;
+    //         }
+    //         else{
+    //            if(cnt==indx1)ele1=nums2[j];
+    //            if(cnt==indx2)ele2=nums2[j];
+    //            cnt++;
+    //            j++;
+    //         }
+    //     }
+    //     while(i<n1){
+    //          if(cnt==indx1)ele1=nums1[i];
+    //            if(cnt==indx2)ele2=nums1[i];
+    //            cnt++;
+    //            i++;
+    //     }
+    //     while(j<n2){
+    //          if(cnt==indx1)ele1=nums2[j];
+    //            if(cnt==indx2)ele2=nums2[j];
+    //            cnt++;
+    //            j++;
+    //     }
+    //     if(n%2==1)return ele1;
+    //    return (double)(ele1+ele2)/2.0;
+    int n1=nums1.length;
+    int n2=nums2.length;
+    if(n1>n2)return findMedianSortedArrays(nums2,nums1);
+    int n=n1+n2;
+    int section=(n1+n2+1)/2;
+    int low=0;
+   int high=n1;
+    
+    while(low<=high){
+        int mid1=low+(high-low)/2;
+        int mid2=section-mid1;
+        int l1=Integer.MIN_VALUE;
+        int l2=Integer.MIN_VALUE;
+        int r1=Integer.MAX_VALUE;
+        int r2=Integer.MAX_VALUE;
+        if(mid1<n1)r1=nums1[mid1];
+        if(mid2<n2)r2=nums2[mid2];
+        if(mid1-1>=0)l1=nums1[mid1-1];
+        if(mid2-1>=0)l2=nums2[mid2-1];
+        if(l1<=r2 && l2<=r1){
+         if(n%2==1){
+            return Math.max(l1,l2);
+         }
+         return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+        }
+        else if(l1>r2){
+            high=mid1-1;
+        }
+        else{
+            low=mid1+1;
+        }
+    }
+    return -1;
+    }
+}
